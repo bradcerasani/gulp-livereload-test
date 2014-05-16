60fc36b32439d103ad92de86531f50ccfb7a0f46
Brad Cerasani <bradcerasani@gmail.com>
2014-05-16 00:29:50 -0500

    replaced gulp-livereload with gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var csso = require('gulp-csso');
-var livereload = require('gulp-livereload');
-var http = require('http');
-var path = require('path');
 var rename = require('gulp-rename');
+var connect = require('gulp-connect');
 
 gulp.task('default', function() {
-  var connect = require('connect');
-  var server = connect();
-
-  server.use(connect.static('./')).listen(3000);
+  connect.server({
+    livereload: true
+  });
   gulp.watch('sass/*.scss', ['styles']);
 });
 
@@ -18,7 +15,7 @@ gulp.task('styles', function() {
   return gulp.src('./sass/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./css'))
-    .pipe(livereload())
+    .pipe(connect.reload())
     .pipe(csso())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./css'))
